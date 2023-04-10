201c98a66631d4344f1a30b99d5c2b61c6721ab1
renatopag-ai <renato.oliveira@pag.ai>
2023-04-09 23:58:13 -0300

    create hook pre-commit

--- gitflow_hook.sh
+++ gitflow_hook.sh
@@ -44,3 +44,42 @@ else
   exit 1
 fi
 
+# Comandos e subcomandos permitidos do Git Flow
+declare -A ALLOWED_COMMANDS
+ALLOWED_COMMANDS=(
+  ["init"]="init"
+  ["feature"]="start|finish"
+  ["bugfix"]="start|finish"
+  ["release"]="start|finish"
+  ["hotfix"]="start|finish"
+  ["support"]="start"
+)
+
+# Função para verificar se o comando e subcomando fornecidos são permitidos
+is_allowed_command() {
+  local cmd=$1
+  local subcmd=$2
+  for allowed_cmd in "${!ALLOWED_COMMANDS[@]}"; do
+    if [[ $cmd == $allowed_cmd && $subcmd =~ ${ALLOWED_COMMANDS[$cmd]} ]]; then
+      return 0
+    fi
+  done
+  return 1
+}
+
+# Captura o comando e subcomando digitados no terminal
+echo "Por favor, digite seu comando do Git Flow:"
+read -e GIT_FLOW_COMMAND
+
+# Extrai o comando e subcomando
+IFS=' ' read -ra CMD_PARTS <<< "$GIT_FLOW_COMMAND"
+COMMAND="${CMD_PARTS[0]}"
+SUBCOMMAND="${CMD_PARTS[1]}"
+
+# Verifica se o comando e subcomando fornecidos são permitidos
+if is_allowed_command "$COMMAND" "$SUBCOMMAND"; then
+  echo "Comando permitido."
+else
+  echo "Comando não permitido. Por favor, use apenas comandos e subcomandos do Git Flow."
+  exit 1
+fi
